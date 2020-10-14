148303392e4734394c82568b01893b36fcc2acc0
masb0ymas <fajriajjha@gmail.com>
2020-10-14 21:44:47 +0700

    refactor: default header

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import {
   Badge,
@@ -8,74 +8,29 @@ import {
   DropdownToggle,
   Nav,
   NavItem,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Spinner,
-  FormGroup,
 } from 'reactstrap'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { Formik, Form, Field } from 'formik'
-import * as Yup from 'yup'
-
 import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react'
-import { signOut, changePass } from '../../modules/auth/actions'
 import logo from '../../assets/img/brand/logo.svg'
 import sygnet from '../../assets/img/brand/sygnet.svg'
-import { CfInput } from '../../components'
-import { userData } from '../../helpers'
+import withToggle, { WithToggleProps } from '../../HOC/withToggle'
+import ModalForm from './ModalForm/ModalForm'
 
-const initialValues = {
-  currentPassword: '',
-  password: '',
-  confirmPassword: '',
-}
+function DefaultHeader(props) {
+  // eslint-disable-next-line
+  const { modalForm, isLoading, signOut, className, children, ...attributes } = props
 
-const changePassSchema = Yup.object().shape({
-  currentPassword: Yup.string().required('password saat ini belum diisi'),
-  password: Yup.string().required('password belum diisi'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'ulangi password tidak cocok')
-    .required('ulangi password belum diisi'),
-})
+  return (
+    <>
+      <AppSidebarToggler className="d-lg-none" display="md" mobile />
+      <AppNavbarBrand
+        full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
+        minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+      />
+      <AppSidebarToggler className="d-md-down-none" display="lg" />
 
-class DefaultHeader extends Component {
-  state = {
-    modal: false,
-  }
-
-  toggle = () => {
-    const { modal } = this.state
-    this.setState({
-      modal: !modal,
-    })
-  }
-
-  handleChangePassword = (values) => {
-    const { changePass } = this.props
-    const { _id } = userData()
-    console.log('Change Password', values)
-    changePass(values, _id)
-  }
-
-  render() {
-    const { modal } = this.state
-    // eslint-disable-next-line
-    const { isLoading, signOut, className, children, ...attributes } = this.props
-
-    return (
-      <>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
-        />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-
-        {/* <Nav className="d-md-down-none" navbar>
+      {/* 
+        <Nav className="d-md-down-none" navbar>
           <NavItem className="px-3">
             <NavLink to="/dashboard" className="nav-link" >Dashboard</NavLink>
           </NavItem>
@@ -85,142 +40,65 @@ class DefaultHeader extends Component {
           <NavItem className="px-3">
             <NavLink to="#" className="nav-link">Settings</NavLink>
           </NavItem>
-        </Nav> */}
-        <Nav className="ml-auto" navbar>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link">
-              <i className="icon-bell" />
-              <Badge pill color="danger">
-                5
-              </Badge>
-            </NavLink>
-          </NavItem>
+        </Nav> 
+      */}
 
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link">
-              Hallo, Admin
-            </NavLink>
-          </NavItem>
+      <Nav className="ml-auto" navbar>
+        <NavItem className="d-md-down-none">
+          <NavLink to="#" className="nav-link">
+            <i className="icon-bell" />
+            <Badge pill color="danger">
+              5
+            </Badge>
+          </NavLink>
+        </NavItem>
 
-          <UncontrolledDropdown nav direction="down">
-            <DropdownToggle nav>
-              <img
-                src="../../assets/img/avatars/6.jpg"
-                className="img-avatar"
-                alt="admin@bootstrapmaster.com"
-              />
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem header tag="div" className="text-center">
-                <strong>Account</strong>
-              </DropdownItem>
-              <DropdownItem onClick={this.toggle}>
-                <i className="fa fa-user" />
-                &nbsp; Change Password
-              </DropdownItem>
-              <DropdownItem onClick={signOut}>
-                <i className="fa fa-lock" />
-                &nbsp; Logout
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-        {/* <AppAsideToggler className="d-md-down-none" /> */}
-        {/* <AppAsideToggler className="d-lg-none" mobile /> */}
+        <NavItem className="d-md-down-none">
+          <NavLink to="#" className="nav-link">
+            Hallo, Admin
+          </NavLink>
+        </NavItem>
 
-        <Modal isOpen={modal} toggle={this.toggle} backdrop="static" className={className}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={changePassSchema}
-            onSubmit={(values, actions) => {
-              setTimeout(() => {
-                this.handleChangePassword(values)
-                actions.setSubmitting(false)
-              }, 1000)
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <ModalHeader toggle={this.toggle}>Form Change Password</ModalHeader>
-                <ModalBody>
-                  <FormGroup>
-                    <Field
-                      label="Password saat ini"
-                      isRequired
-                      type="password"
-                      name="currentPassword"
-                      placeholder="*******"
-                      component={CfInput}
-                    />
-                  </FormGroup>
+        <UncontrolledDropdown nav direction="down">
+          <DropdownToggle nav>
+            <img
+              src="../../assets/img/avatars/6.jpg"
+              className="img-avatar"
+              alt="admin@bootstrapmaster.com"
+            />
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem header tag="div" className="text-center">
+              <strong>Account</strong>
+            </DropdownItem>
+            <DropdownItem onClick={modalForm.toggle}>
+              <i className="fa fa-user" />
+              &nbsp; Change Password
+            </DropdownItem>
+            <DropdownItem onClick={signOut}>
+              <i className="fa fa-lock" />
+              &nbsp; Logout
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
 
-                  <FormGroup>
-                    <Field
-                      label="Password"
-                      isRequired
-                      type="password"
-                      name="password"
-                      placeholder="*******"
-                      component={CfInput}
-                    />
-                  </FormGroup>
+      {/* <AppAsideToggler className="d-md-down-none" /> */}
+      {/* <AppAsideToggler className="d-lg-none" mobile /> */}
 
-                  <FormGroup>
-                    <Field
-                      label="Ulangi Password"
-                      isRequired
-                      type="password"
-                      name="confirmPassword"
-                      placeholder="*******"
-                      component={CfInput}
-                    />
-                  </FormGroup>
-                </ModalBody>
-                <ModalFooter>
-                  <Button type="button" color="secondary" onClick={this.toggle}>
-                    Cancel
-                  </Button>
-                  &nbsp;
-                  <Button
-                    type="submit"
-                    color="primary"
-                    className="px-4"
-                    disabled={isSubmitting || isLoading}
-                  >
-                    {isSubmitting || isLoading ? (
-                      <>
-                        <Spinner size="sm" color="light" />
-                        &nbsp;Loading...
-                      </>
-                    ) : (
-                      'Save Changes'
-                    )}
-                  </Button>
-                </ModalFooter>
-              </Form>
-            )}
-          </Formik>
-        </Modal>
-      </>
-    )
-  }
+      <ModalForm {...modalForm} className={className} />
+    </>
+  )
 }
 
 DefaultHeader.propTypes = {
   children: PropTypes.node,
-  changePass: PropTypes.func,
+  modalForm: WithToggleProps,
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth.authenticated,
-  message: state.auth.message,
-  success: state.auth.success,
-  isLoading: state.auth.isLoading,
+export default withToggle({
+  Component: DefaultHeader,
+  toggles: {
+    modalForm: false,
+  },
 })
-
-const mapDispatchToProps = (dispatch) => ({
-  changePass: (rowData, id) => dispatch(changePass(rowData, id)),
-  signOut: () => dispatch(signOut()),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(DefaultHeader)
