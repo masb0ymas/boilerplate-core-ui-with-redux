163c67da9d0430a41bfae01ba5957410a25af426
masb0ymas <fajriajjha@gmail.com>
2020-10-24 16:29:29 +0700

    fix: add format stand alone

--- src/helpers/Date.js
+++ src/helpers/Date.js
@@ -31,6 +31,10 @@ const formatTime = (date) => {
   return format(new Date(date), 'HH:mm:ss', timezoneIndonesia)
 }
 
+const formatMonthStandAlone = (date) => {
+  return format(new Date(date), 'LLLL', timezoneIndonesia)
+}
+
 export {
   formatDate,
   formatDateSystem,
@@ -39,4 +43,5 @@ export {
   formatMonth,
   formatYear,
   formatTime,
+  formatMonthStandAlone,
 }
