df62050242cca035e8f33e50edc7e7fc7cd0c962
masb0ymas <fajriajjha@gmail.com>
2020-10-28 21:35:59 +0700

    feat: add component loading

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loading.css'
+
+function Loading() {
+  return (
+    <div className="sk-chase">
+      <div className="sk-chase-dot" />
+      <div className="sk-chase-dot" />
+      <div className="sk-chase-dot" />
+      <div className="sk-chase-dot" />
+      <div className="sk-chase-dot" />
+      <div className="sk-chase-dot" />
+    </div>
+  )
+}
+
+export default Loading
