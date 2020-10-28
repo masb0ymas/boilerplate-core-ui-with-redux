f75686a8dfbf44c5d53be6c538775b969a2923e5
masb0ymas <fajriajjha@gmail.com>
2020-10-28 13:59:43 +0700

    remove unuse function

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -31,12 +31,6 @@ const DefaultHeader = React.lazy(() => import('./DefaultHeader'))
 class DefaultLayout extends Component {
   loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
 
-  signOut(e) {
-    e.preventDefault()
-    const { history } = this.props
-    history.push('/login')
-  }
-
   render() {
     const { isVerify, auth } = this.props
 
@@ -50,7 +44,7 @@ class DefaultLayout extends Component {
       <div className="app">
         <AppHeader fixed>
           <Suspense fallback={this.loading()}>
-            <DefaultHeader onLogout={(e) => this.signOut(e)} />
+            <DefaultHeader />
           </Suspense>
         </AppHeader>
         <div className="app-body">
